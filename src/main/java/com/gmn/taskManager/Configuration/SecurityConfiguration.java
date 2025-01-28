32e6cb04a0db55c4a30e5ffae453bb51542ba31f
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-01-28 19:13:04 +0530

    Add Security FilterChain

--- src/main/java/com/gmn/taskManager/Configuration/SecurityConfiguration.java
+++ src/main/java/com/gmn/taskManager/Configuration/SecurityConfiguration.java
@@ -1,5 +1,25 @@
 package com.gmn.taskManager.Configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SecurityConfiguration {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                        request.requestMatchers("login", "register")
+                                .permitAll()
+                                .anyRequest().authenticated())
+                .httpBasic((Customizer.withDefaults()))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
 }
