62ad9b735608f734f43146059dd23972860bdc33
MacBook <macbook@MacBooks-MacBook-Pro.local>
2025-01-29 15:57:20 -0800

    gowri

---
+++ src/main/java/com/gmn/taskManager/DTO/TaskDTO.java
@@ -0,0 +1,68 @@
+package com.gmn.taskManager.DTO;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+public class TaskDTO {
+
+    @Id
+    @JsonIgnore
+    private String taskId;
+    private String taskName;
+    private String taskStatus;
+    private LocalDate startDate;
+    private LocalDate endDate;
+
+    public TaskDTO() {
+    }
+
+    public TaskDTO(String taskId, String taskName, String taskStatus, LocalDate startDate, LocalDate endDate) {
+        this.taskId = taskId;
+        this.taskName = taskName;
+        this.taskStatus = taskStatus;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+}
